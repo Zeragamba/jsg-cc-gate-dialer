98d4e787e436ce13262dc801b07ef43e4399b65a
Zeragamba <code@Zeragamba.ca>
2025-03-14 23:41:00 -0400

    v3.0.0-rc

---
+++ src/address-book.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        name = "Abydos",
+        type = "MilkyWay",
+        addresses = {
+            MilkyWay = { "Taurus", "Serpens Caput", "Capricornus", "Monoceros", "Sagittarius", "Orion" },
+        }
+    }, {
+        name = "Example", -- Name of the stargate
+        type = "MilkyWay", -- The type of the gate, to determine if a 7 symbol address can be used
+        addresses = {
+            -- Addresses for the gate based on glyph type
+            MilkyWay = { "Taurus", "Serpens Caput", "Capricornus", "Monoceros", "Sagittarius", "Orion" },
+            Pegasus = { "Zamilloz", "Ecrumig", "Sandovi", "Recktic", "Dawnre", "Sibbron" },
+            Universe = { 23, 17, 8, 15, 20, 21 },
+        }
+    }
+}
---
+++ src/ssgd-install.lua
@@ -0,0 +1,21 @@
+print("== SSGD-CC installer ==")
+
+local raw_files_url = "https://raw.githubusercontent.com/Zeragamba/jsg-cc-gate-dialer"
+local ref_name = 'main'
+
+local function download_file(src_path, dest)
+    local src_url = raw_files_url .. "/refs/heads/" .. ref_name .. "/src/" .. src_path
+    local dest_file = shell.resolve(dest or src_path)
+    shell.execute("wget", src_url, dest_file)
+end
+
+download_file('ssgd.lua')
+
+if fs.exists('address-book.lua') then
+
+    download_file("address-book.lua")
+end
+
+print("== SSGD-CC installed! ==")
+print("run the following command to edit your address book:")
+print("  edit address-book.lua")
